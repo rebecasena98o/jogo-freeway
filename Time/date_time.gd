13d07871129d3a47d8b10acde5dde903f7798782
Reb Sena <r3b3casena@gmail.com>
2025-10-15 23:07:02 -0300

    Add files via upload

--- Time/date_time.gd
+++ Time/date_time.gd
@@ -1,1 +1,10 @@
+class_name DateTime extends Resource 
 
+@export var seconds = 0
+@export var minutes = 0
+
+@export_range(0,5) var default_minutes := 2 
+@export_range(0,59) var default_seconds := 0
+
+func _ready() -> void:
+	Timer_counter.text = str("%02d" % default_minutes)
